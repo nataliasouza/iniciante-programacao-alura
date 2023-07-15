21c6945c71eac4e7ca196d49a9d8940a5019609b
Natalia Souza <nataliasouza.ti@hotmail.com>
2023-07-15 19:41:50 -0300

    Eventos do teclado

--- main.js
+++ main.js
@@ -12,5 +12,17 @@ for(let contador = 0; contador < listaDeTeclas.length; contador++){
 
     tecla.onclick = function () {
         tocarSom(idAudio)
-    }   
+    }
+    
+    tecla.onkeydown = function (evento) {
+        console.log(evento.code)
+        
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }
